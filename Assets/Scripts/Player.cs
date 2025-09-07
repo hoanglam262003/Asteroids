af963595c74cc2e8d634ed785f1b5534bd096203
Vu Le Lam Hoang <hoanglam262003@gmail.com>
2025-09-07 09:26:56 +0700

    player movement and shooting

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,57 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class Player : MonoBehaviour
+{
+    public Bullet bullet;
+    public float thrustForce = 1.0f;
+    public float turnForce = 1.0f;
+    private Rigidbody2D rb;
+    private bool thrusting;
+    private float turnDirection;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+        thrusting = Keyboard.current.wKey.isPressed || Keyboard.current.upArrowKey.isPressed;
+        if (Keyboard.current.aKey.isPressed || Keyboard.current.leftArrowKey.isPressed)
+        {
+            turnDirection = 1.0f;
+        }
+        else if (Keyboard.current.dKey.isPressed || Keyboard.current.rightArrowKey.isPressed)
+        {
+            turnDirection = -1.0f;
+        }
+        else
+        {
+            turnDirection = 0.0f;
+        }
+
+        if (Keyboard.current.spaceKey.wasPressedThisFrame || Mouse.current.leftButton.wasPressedThisFrame)
+        {
+            Shoot();
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        if (thrusting)
+        {
+            rb.AddForce(transform.up * thrustForce);
+        }
+        if (turnDirection != 0.0f)
+        {
+            rb.AddTorque(turnDirection * turnForce);
+        }
+    }
+
+    private void Shoot()
+    {
+        Bullet bullet = Instantiate(this.bullet, this.transform.position, this.transform.rotation);
+        bullet.Project(this.transform.up);
+    }
+}
